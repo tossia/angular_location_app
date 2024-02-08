09cd707c9ae6df250cf1895ecfec594f4b24d849
antonina_ng <antonina.nguyen@fms-ea.com>
2024-02-08 18:30:47 +0100

    replace template by templateUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,7 @@ import { HomeComponent } from './home/home.component';
     RouterLink,
     RouterOutlet,
   ],
-  template: `
-    <main>
-      <a [routerLink]="['/']">
-        <header class="brand-name">
-          <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
-        </header>
-      </a>
-      <section class="content">
-        <router-outlet></router-outlet>
-      </section>
-    </main>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
